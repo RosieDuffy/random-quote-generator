0b5ec845e317ce28a6badc5a77eb47615cc6b9ae
RosieDuffy <rose.j.duffy@gmail.com>
2023-07-21 11:31:37 +0100

    Quotes added

--- js/script.js
+++ js/script.js
@@ -11,6 +11,42 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+let quotes = [
+  {quote: 'My girlfriend is absolutely beautiful. Body like a Greek statue...completely pale, no arms.', 
+   source: 'Phil Wang',
+   citation: 'Fest Magazine',
+   year: 2019},
+  
+
+  {quote: 'I bought myself some glasses. My observational comedy improved.', 
+   source: 'Sara Pascoe'},
+
+  {quote: 'The first time I met my wife, I knew she was a keeper. She was wearing massive gloves.', 
+   source: 'Alun Cochrane',
+   citation: 'Fringe Festival',
+   year: 2015},
+
+  {quote: "As a kid I was made to walk the plank. We couldn't afford a dog.", 
+   source:'Gary Delaney'},
+
+  {quote: 'I saw a documentary on how ships are kept together. Riveting!', 
+   source:'Stewart Francis',
+   citation: 'Twitter',
+   year: 2006 },
+
+  {quote: "My therapist says I have a preoccupation with vengeance. We'll see about that.",
+   source:'Stewart Francis'},
+
+  {quote: "I was in my car driving back from work. A police officer pulled me over and knocked on my window. I said, 'One minute I'm on the phone.'", 
+   source: 'Alan Carr',
+   citation: 'Rolling Stone Magazine',
+   year: 2020},
+
+  {quote: "Here's a picture of me with REM. That's me in the corner.", 
+   source: 'Milton Jones'}
+]
+
+
 
 
 /***
